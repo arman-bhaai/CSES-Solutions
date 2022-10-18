a6f49473ead0eca86448c16620d0aa5c1010b29b
arman-bhaai <arman.bhaai@gmail.com>
2022-10-18 22:35:02 +0600

    added :: category --> Introductory Problems

---
+++ CSES Palindrome Reorder.cpp
@@ -0,0 +1,110 @@
+/*______________________________________________________________________
+||--------------------------------------------------------------------||
+||                                                                    ||
+|| "I bear witness that there is no god but Allah and I bear witness  ||
+||    that Muhammad (peace be upon him) is the Messenger of Allah"    ||
+||                                                                    ||
+|| *** In the name of Allah, the Most Gracious, the Most Merciful.*** ||
+||                                                                    ||
+========================================================================
+========================================================================
+||                                                                    ||
+|| --> Author     :     Abu Bakar Siddique Arman (#arman_bhaai)       ||
+|| --> Email      :     arman.bhaai@gmail.com                         ||
+|| --> Portfolio  :     arman-bhaai.github.io                         ||
+|| --> LinkedIn   :     linkedin.com/in/abubakar-arman                ||
+|| --> GitHub     :     github.com/arman-bhaai                        ||
+|| --> Facebook   :     fb.me/arman.bhaai                             ||
+|| --> StopStalk  :     stopstalk.com/user/profile/arman_bhaai        ||
+|| --> Clist      :     clist.by/coder/arman_bhaai                    ||
+|| --> CodeForces :     codeforces.com/profile/arman_bhaai            ||
+|| --> CodeChef   :     codechef.com/users/arman_bhaai                ||
+|| --> AtCoder    :     atcoder.jp/users/arman_bhaai                  ||
+|| --> HackerRank :     hackerrank.com/arman_bhaai                    ||
+|| --> LeetCode   :     leetcode.com/arman_bhaai                      ||
+||____________________________________________________________________||
+----------------------------------------------------------------------*/
+
+// I believe in OpenSource. So, any of my code snippets are Copyright-Free.
+// <3 Happy Coding <3
+
+// Contest ID   :: CSES
+// Problem Name :: Palindrome Reorder
+// Problem URL  :: https://cses.fi/problemset/result/4668814/
+
+/*******************************************************************************
+////////////////////////////////////////////////////////////////////////////////
+*******************************************************************************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef string str;
+typedef vector<int> vi;
+typedef vector<ll> vii;
+typedef vector<vector<ll>> vvi;
+typedef map<int,int> mi;
+typedef map<ll,ll> mii;
+
+// shortcuts
+#define endl '\n'
+#define F first
+#define S second
+#define getv(v) for(auto &k:v) cin>>k;
+#define all(v) v.begin(),v.end()
+// debugging
+#define couts(i) cout<<i<<" ";
+#define coutn(i) cout<<i<<'\n';
+#define showv(v) cout<<endl;for(auto &k:v) cout<<k<<", ";cout<<endl;
+#define showm(m) cout<<endl;for(auto &k:m) cout<<k.F<<" -> "<<k.S<<endl;cout<<endl;
+
+void optimize();
+// void solve();
+
+
+void solve(){
+	string s, s2="", sm=""; cin>>s;
+ 
+	for(int i='A'; i<='Z'; i++){
+		int ct = count(s.begin(), s.end(), i);
+		if(ct>0){
+			if(ct%2==0){
+				for(int j=0; j<ct/2; j++) s2.push_back(i);
+			} else if(ct==1) sm.push_back(i);
+			else {
+				for(int j=0; j<(ct-1)/2; j++) s2.push_back(i);
+				sm.push_back(i);
+			}
+		}
+	}
+	string s3 = s2;
+	reverse(s3.begin(), s3.end());
+	string pld = s2+sm+s3, pld2 = pld;
+	reverse(pld2.begin(), pld2.end());
+	if(pld == pld2){cout<<pld; return;}
+	cout<<"NO SOLUTION";
+	// cout<<count(s.begin(), s.end(), 'A');
+}
+
+int main(){
+	optimize();
+	int __=1;
+	// cin>>__;
+	for(int _=1; _<=__; _++){
+		// cout<<"Case "<<_<<": ";
+		solve(); cout<<endl;
+	}
+	return 0;
+}
+
+
+void optimize(){
+	// fastIO
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+}
+
+/* Alternatives
+*/
